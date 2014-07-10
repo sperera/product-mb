71d6266398876db36aeb8a9c4720d2f5a0c0b497
Sewwandi Perera <sew.tweety@gmail.com>
2014-07-10 12:02:57 +0530

    Test case for MB 554: TO BE EDITED

--- modules/integration/tests-integration/tests-server/src/test/java/org/wso2/mb/integration/tests/server/mgt/RenameVirtualHostTestCase.java
+++ modules/integration/tests-integration/tests-server/src/test/java/org/wso2/mb/integration/tests/server/mgt/RenameVirtualHostTestCase.java
@@ -1,49 +1,47 @@
 package org.wso2.mb.integration.tests.server.mgt;
 
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
-import org.wso2.carbon.automation.core.ProductConstant;
-import org.wso2.carbon.automation.core.utils.environmentutils.EnvironmentBuilder;
-import org.wso2.carbon.integration.common.admin.client.LogViewerClient;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import org.wso2.carbon.automation.api.clients.authenticators.AuthenticatorClient;
+import org.wso2.carbon.automation.engine.context.TestUserMode;
+import org.wso2.carbon.automation.engine.frameworkutils.FrameworkPathUtil;
 import org.wso2.carbon.integration.common.utils.mgt.ServerConfigurationManager;
+import org.wso2.mb.integration.common.clients.operations.utils.AndesClientUtils;
 import org.wso2.mb.integration.common.utils.backend.MBIntegrationBaseTest;
 
 import java.io.File;
 
 public class RenameVirtualHostTestCase extends MBIntegrationBaseTest {
-
-    private EnvironmentBuilder builder = null;
+    private static final Log log = LogFactory.getLog(RenameVirtualHostTestCase.class);
     private ServerConfigurationManager serverManager = null;
-    private LogViewerClient logViewerClient =null;
 
-    @BeforeTest(alwaysRun = true)
-    public void startJMSBroker() throws Exception {
-
-        builder = new EnvironmentBuilder().mb(ProductConstant.SUPER_ADMIN_USER_ID);
-        serverManager = new ServerConfigurationManager(automationContext);
-
-        if (builder.getFrameworkSettings().getEnvironmentSettings().is_builderEnabled()) {
-
-            //changing the andes-virtualhosts.xml with the new configuration and restarts the server
-            serverManager.applyConfiguration(new File(ProductConstant.getResourceLocations(ProductConstant.MB_SERVER_NAME)
-                    + File.separator + "configs" + File.separator + "advanced" + "andes-virtualhosts.xml"));
-
-        }
-
-        /*
-        FrameworkProperties properties = FrameworkFactory.getFrameworkProperties(ProductConstant.MB_SERVER_NAME);
-        UserInfo userInfo = UserListCsvReader.getUserInfo( ProductConstant.SUPER_ADMIN_USER_ID);
-        logViewerClient = new LogViewerClient(properties.getProductVariables().getBackendUrl(),
-                userInfo.getUserName(), userInfo.getPassword());
-                */
+    @BeforeClass(alwaysRun = true)
+    public void init() throws Exception {
+        super.init(TestUserMode.SUPER_TENANT_USER);
+        AndesClientUtils.sleepForInterval(15000);
     }
 
-    @AfterTest(alwaysRun = true)
-    public void stopJMSBroker() throws Exception {
-        if (builder.getFrameworkSettings().getEnvironmentSettings().is_builderEnabled()) {
-            if (serverManager != null) {
-                serverManager.restoreToLastConfiguration();
-            }
-        }
+    @Test(groups = "wso2.mb", description = "Change the andes-virtualhosts config to rename the existing virtual host and start MB")
+    public void changeVirtualHostConfigAndStartMB() throws Exception {
+
+        //changing the andes-virtualhosts.xml with the new configuration and restarts the server
+        serverManager = new ServerConfigurationManager(automationContext);
+        serverManager.applyConfiguration(new File(FrameworkPathUtil.getSystemResourceLocation() + File.separator
+                + "artifacts" + File.separator + "MB" + File.separator + "configs" + File.separator + "advanced"
+                + File.separator + "andes-virtualhosts.xml"),
+                new File(ServerConfigurationManager.getCarbonHome() + File.separator + "repository" + File.separator
+                +"conf"+ File.separator + "advanced" + File.separator + "andes-virtualhosts.xml") , true, true);
+
+        // Authenticating to the started MB server
+        AuthenticatorClient authenticatorClient =
+                new AuthenticatorClient(automationContext.getContextUrls().getServiceUrl());
+        authenticatorClient.login(automationContext.getSuperTenant().getContextUser().getUserName(),
+                automationContext.getSuperTenant().getContextUser().getPassword(), "127.0.0.1");
+        log.info("Succesfully logged into MB.");
+
+        // Restoring the andes-virtualhosts config
+        serverManager.restoreToLastConfiguration();
     }
 }
